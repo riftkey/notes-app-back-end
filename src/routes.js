a44250411cb9f02ee2ff417f759bc913f5f1e39a
riftkey <rifqimatondang@gmail.com>
2023-03-25 12:37:30 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler,getNoteByIdHandle,editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+      },
+      {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandle,
+      },
+      {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+      },
+      {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+      },
+  ];
+   
+  module.exports = routes;
